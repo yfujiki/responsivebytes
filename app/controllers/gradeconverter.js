655529be205304732b91656e2e2bf3e48e3c622c
Yuichi Fujiki <yuichi@chaione.com>
2017-06-25 21:05:41 -0500

    Show conversion modal.

--- app/controllers/gradeconverter.js
+++ app/controllers/gradeconverter.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    converterDisplayMode: "modal-hidden",
+    actions: {
+        openModalConverter() {
+            console.log("Open");
+            this.set('converterDisplayMode', "modal-show");
+        },
+        closeModalConverter() {
+            console.log("Close")
+            this.set('converterDisplayMode', 'modal-hidden');
+        }
+    }
 });
