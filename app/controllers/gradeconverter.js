9bf6e6faeb773707cf17a05a3cf2dbf2e8ee7435
Yuichi Fujiki <yuichi@chaione.com>
2017-06-26 21:43:59 -0500

    Show convert result

--- app/controllers/gradeconverter.js
+++ app/controllers/gradeconverter.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     converterDisplayMode: "modal-hidden",
     selectedFromSystem: "",
-    selectedFromSystemGrades: [],
+    selectedFromGrades: [],
+    selectedFromGrade: "",
+    selectedToSystem: "",
+    convertedGrade: "",
+    convertedResultMode: "convert-result-hide",
     actions: {
         openModalConverter() {
             this.set('converterDisplayMode', "modal-show");
@@ -19,7 +23,48 @@ export default Ember.Controller.extend({
 
             let model = this.get('model');
             let table = model.table;
-            this.set('selectedFromSystemGrades', table.distinctGradesForSystem(selectedSystem));
+            this.set('selectedFromGrades', table.distinctGradesForSystem(selectedSystem));
+        },
+        toSystemSelected() {
+            let selectedElement = Ember.$('#toSystem')[0];
+            let selectedIndex = selectedElement.selectedIndex;
+            let selectedSystem = selectedElement.options[selectedIndex].value;
+            this.set('selectedToSystem', selectedSystem);
+        },
+        fromGradeSelected() {
+            let selectedElement = Ember.$('#fromGrade')[0];
+            let selectedIndex = selectedElement.selectedIndex;
+            let selectedSystem = selectedElement.options[selectedIndex].value;
+            this.set('selectedFromGrade', selectedSystem);            
+        },
+        convert() {
+            let model = this.get('model');
+            let table = model.table;
+
+            let fromGrades = Ember.$("#fromGrade")[0];
+            var selectedFromGradeIndex = fromGrades.selectedIndex;
+            var selectedGradeIndex = 0;
+            if (selectedFromGradeIndex === -1) {
+                fromGrades.selectedIndex = 0                
+                selectedFromGradeIndex = 0;
+                this.actions.fromSystemSelected.bind(this)();
+                selectedGradeIndex = 0;
+            } else {
+                var selectedFromGrade = fromGrades.options[selectedFromGradeIndex].value;
+                let selectedElement = Ember.$('#fromSystem')[0];
+                let selectedIndex = selectedElement.selectedIndex;
+                let selectedSystem = selectedElement.options[selectedIndex].value;
+                selectedGradeIndex = table.systems[selectedSystem].grades.indexOf(selectedFromGrade);
+            }
+
+            let toSystems = Ember.$("#toSystem")[0];
+            let selectedToSystemIndex = toSystems.selectedIndex;
+            let selectedToSystem = toSystems.options[selectedToSystemIndex].value;
+            let toGrades = table.systems[selectedToSystem].grades;
+            let convertedGrade = toGrades[selectedGradeIndex];
+            this.set('convertedGrade', convertedGrade);
+            let resultMode = (convertedGrade.length > 0) ? "convert-result-show" : "convert-result-hide"
+            this.set('convertedResultMode', resultMode);
         }
     }
 });
